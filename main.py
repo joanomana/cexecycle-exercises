db2805a04d9cbaf836e675b3f0ed6b663c19d2b8
“joanomana” <joansebastianomasu@gmail.com>
2024-10-31 17:23:28 -0500

    Solucion ejercicio calculadora

---
+++ main.py
@@ -0,0 +1,6 @@
+number = int(input("Enter a number to calculate the table from 1 to 10: "))
+calculate =1
+while calculate <=10:
+    result= number * calculate
+    print(f"{number} x {calculate} = {result}")
+    calculate +=1
