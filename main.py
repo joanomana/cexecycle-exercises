37a0870ac43d9d86ed77d9cd993b854b18cc6743
“joanomana” <joansebastianomasu@gmail.com>
2024-10-31 17:54:47 -0500

    Solucion ejercicio suma entre numeros

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-import math
-num1=int(input("Enter a number: "))
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 number = 0
-while number <= num1:
-    result = math.pow(2,number)
-    print(round(result))
-    number +=1
+while number <= num2:
+
+    if num1 < num2:
+        number = num1 + 1
+        total_sum = 0
+        while number < num2:
+            total_sum += number
+            number += 1
+        print(f"The sum of numbers between {num1} and {num2} is {total_sum}")
+        break
+    else:
+        print("The first number should be less than the second number.")
+        break
