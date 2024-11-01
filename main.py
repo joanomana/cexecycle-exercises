fb33e80c58356c462ab36bb8d0467024d72f20b3
“joanomana” <joansebastianomasu@gmail.com>
2024-10-31 19:08:51 -0500

    Solucion ejercicio asteriscos

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
-minutes = int(input("Enter the time: "))
-total_minutes = 0
+print("Draw a rectangle!")
+height = int(input("Enter the height: "))
+broad = int(input("Enter the broad: "))
 
-while minutes > 0:
-    total_minutes += minutes
-    minutes = int(input("Enter the time: "))
+rectangle = (height * broad) /3
+for i in range(height):
+    for j in range(broad):
+        print("*", end="")
+    print("")
 
-hours = total_minutes // 60
-minutes = total_minutes % 60
-print(f"Total travel time: {hours} hours and {minutes} minutes")
+
+
+print("")
+print("Draw a triangle")
+height = int(input("Enter the height: "))
+
+for i in range(height):
+    for j in range(i + 1):
+        print("*", end="")
+    print("")
+
+print("")
+print("Draw a hexagon")
+size = int(input("Enter the size: "))
+
+# Upper part of the hexagon
+for i in range(size):
+    print(" " * (size - i - 1) + "*" * (size + 2 * i))
+
+# Lower part of the hexagon
+for i in range(size - 1, -1, -1):
+    print(" " * (size - i - 1) + "*" * (size + 2 * i))
